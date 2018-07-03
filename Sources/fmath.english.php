930623a7492300e0ab12f190ea21d162971cbf42
Ian Wild <ian.wild@heavyhorse.co.uk>
2018-07-03 21:16:27 +0100

    Include language file.

---
+++ Sources/fmath.english.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package SMF fMath Mod
+ * @author Lumina Consultancy <https://luminaconsultancy.com>
+ * @copyright 2018
+ * @license GPLv3
+ * @file Mod-fMath.php
+ * @version 0.0.1
+ */
+
+
+$txt['fmath_opentag'] = 'Open tag';
+$txt['fmath_inlineopentag'] = 'Inline open tag';
+$txt['fmath_closetag'] = 'Close tag';
+
+$txt['mods_cat_modifications_fmath'] = 'fMath Editor';
+$txt['fmath_desc'] = 'This mod integrates the fMath editor into an SMF forum. It adds a new \'fMath\' button to the SMF text editor which, when pressed, will display the fMath editor in a pop-up child window. Use the fMath WYSIWYG math notation editor to write LaTeX code and press the Insert button to add the code directly into the SMF text editor. Use these settings to specify the tags used to surround the LaTeX code.'
+?>
