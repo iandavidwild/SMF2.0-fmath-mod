5a3542057750fe430d366112c6a77aca235f1204
Ian Wild <ianwild@luminaconsultancy.com>
2018-08-13 18:22:37 +0100

    First commit of Spanish language files.

---
+++ Sources/fmath.spanish_latin-utf8.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package SMF fMath Mod
+ * @author Lumina Consultancy <https://luminaconsultancy.com>
+ * @copyright 2018
+ * @license GPLv3
+ * @file Mod-fMath.php
+ * @version 1.1.0
+ * Spanish translation (https://www.bombercode.org) Copyright 2014-2018
+ */
+
+
+$txt['fmath_opentag'] = 'Etiqueta abierta';
+$txt['fmath_inlineopentag'] = 'Etiqueta abierta en línea';
+$txt['fmath_closetag'] = 'Cerrar etiqueta';
+
+$txt['mods_cat_modifications_fmath'] = 'Editor fMath';
+$txt['fmath_desc'] = 'Esta modificación integra el editor fMath en un foro SMF. Agrega un nuevo botón &quot;fMath&quot; al editor de texto SMF que, cuando se presiona, mostrará el editor fMath en una ventana secundaria emergente. Use el editor de notación matemática fMath WYSIWYG para escribir el código LaTeX y presione el botón Insertar para agregar el código directamente en el editor de texto SMF. Use estas configuraciones para especificar las etiquetas utilizadas para rodear el código LaTeX.'
+?>
---
+++ Sources/fmath.spanish_latin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package SMF fMath Mod
+ * @author Lumina Consultancy <https://luminaconsultancy.com>
+ * @copyright 2018
+ * @license GPLv3
+ * @file Mod-fMath.php
+ * @version 1.1.0
+ * Spanish translation (https://www.bombercode.org) Copyright 2014-2018
+ */
+
+
+$txt['fmath_opentag'] = 'Etiqueta abierta';
+$txt['fmath_inlineopentag'] = 'Etiqueta abierta en l&iacute;nea';
+$txt['fmath_closetag'] = 'Cerrar etiqueta';
+
+$txt['mods_cat_modifications_fmath'] = 'Editor fMath';
+$txt['fmath_desc'] = 'Esta modificaci&oacute;n integra el editor fMath en un foro SMF. Agrega un nuevo bot&oacute;n &quot;fMath&quot; al editor de texto SMF que, cuando se presiona, mostrar&aacute; el editor fMath en una ventana secundaria emergente. Use el editor de notaci&oacute;n matem&aacute;tica fMath WYSIWYG para escribir el c&oacute;digo LaTeX y presione el bot&oacute;n Insertar para agregar el c&oacute;digo directamente en el editor de texto SMF. Use estas configuraciones para especificar las etiquetas utilizadas para rodear el c&oacute;digo LaTeX.'
+?>
